72e0542a5a579b7290a0fc100acab2116ab77fd0
wangzhangjun <695331215@qq.com>
2014-10-15 09:17:10 +0800

    修改格式问题

--- 第一章/hellomod.c
+++ 第一章/hellomod.c
@@ -2,11 +2,11 @@
 #include<linux/kernel.h>
 #include<linux/init.h>
 
-static int __init lkp_init(void){
+static int __init lkp_init(void) {
 	printk("<1>Hello World!from the kernel space...\n");
 	return 0;
 }
-static void __exit lkp_cleanup(void){
+static void __exit lkp_cleanup(void) {
 	printk("<1>Goodbye,World! leaving kernel space...\n");
 }
 module_init(lkp_init);
